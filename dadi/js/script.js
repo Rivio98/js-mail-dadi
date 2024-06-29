dbd3ddb7105281fe531b1366c8b88bbb1f7f4577
Rivio98 <rivaldogjoni1998@gmail.com>
2024-06-29 14:42:28 +0200

    [UPD] Created button

---
+++ dadi/js/script.js
@@ -0,0 +1,7 @@
+// creiamo un button
+const button = document.getElementById('tira');
+
+//creo un generatore di numeri random da 1 a 6 compreso per giocatore ed computer
+button.addEventListener('click', function () {
+    const
+})
