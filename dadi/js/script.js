5c95574bf39e1be7f7606f78cd0bcfaffd130469
Rivio98 <rivaldogjoni1998@gmail.com>
2024-06-29 14:53:14 +0200

    [UPD] Added if for user result vs pc result

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,7 +3,26 @@ const button = document.getElementById('tira');
 
 //creo un generatore di numeri random da 1 a 6 compreso per giocatore ed computer
 button.addEventListener('click', function () {
+
     //generatore random user
     let user = Math.round(Math.random() * 5) + 1;
     console.log(user)
+
+    //generatore random computer
+    let computer = Math.round(Math.random() * 5) + 1;
+    console.log(computer)
+
+    //all evento click del button questi numeri saranno confrontati:
+    let risultato;
+
+    if (user > computer) {
+        risultato = "Hai vinto"
+    }
+    else if (user < computer) {
+        risultato = "Hai perso"
+    }
+    else {
+        risultato = "Pareggio"
+    }
+    console.log(risultato)
 })
