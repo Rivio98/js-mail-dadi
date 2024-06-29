683f6197d33508acd0b33c85a5b716b65229678e
Rivio98 <rivaldogjoni1998@gmail.com>
2024-06-29 14:45:45 +0200

    [UPD] Random generation user added

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,5 +3,7 @@ const button = document.getElementById('tira');
 
 //creo un generatore di numeri random da 1 a 6 compreso per giocatore ed computer
 button.addEventListener('click', function () {
-    const
+    //generatore random user
+    let user = Math.round(Math.random() * 5) + 1;
+    console.log(user)
 })
