5ed4e5ed321392627594f4ac5f7027454fa7252a
Rivio98 <rivaldogjoni1998@gmail.com>
2024-06-29 12:30:26 +0200

    [UPD] Added variable if the email are in the array the message is : accesso consentito, else : accesso negato

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,21 +1,30 @@
 //creo l' array con dentro varie email
-const autorizzati = ["rivaldogjoni@email.com", "pippobaudo@email.com", "fattigliaffarituoi@gmail.com", "michiamiilpresidente@gmail.com", "maeleiilpresidente@gamail.com", "scalainforza@gmail.com", "nimortaccitua@gmail.com"];
+const autorizzati = ["rivaldogjoni1998@gmail.com", "pippobaudo@gmail.com", "fattigliaffarituoi@gmail.com", "michiamiilpresidente@gmail.com", "maeleiilpresidente@gamail.com", "scalainforza@gmail.com", "nimortaccitua@gmail.com"];
 
 //creo un button, lo recupero dal dom ed aggiungo un evento al click
-const button = document.getElementById("access")
+const button = document.getElementById("access");
 
 button.addEventListener('click', function () {
 
     // creo un input dove chiedo all'utente la sua email ed recupero il valore inserito
-    let email = document.getElementById('email').value.toLowerCase()
+    let email = document.getElementById('email').value.toLowerCase();
 
     //crea una variabile di controllo per dirmi se l'email è presente o meno nell array
     let controllo = false;
 
     //ciclo l'array e confronto l'elemento che sto ciclando con la parola inserita dall'input
     for (let i = 0; i < autorizzati.length; i++) {
-        //se è vera la condizione : Accesso Consentito! imposto la variabile di controllo a true
-        if (autorizzati[i].toLowerCase()) === email) {
+        //se è vera la condizione imposto la variabile di controllo a true
+        if (autorizzati[i].toLowerCase() === email) {
+            controllo = true;
+        }
+    }
+    // se la variabile è true il messaggio visualizzato sarà : Accesso consentito!
+    if (controllo === true) {
+        console.log(`${email.toLowerCase()} Accesso consentito!`);
+    }
+    else { //altrimenti : Accesso negato!
+        console.log(`${email.toLowerCase()} Accesso negato!`);
+    }
+})
 
-}
-    })
